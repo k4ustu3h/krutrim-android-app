be8a49077e9d95072efee30efe8d6997f53f1179
Kaustubh Ladiya <k4ustu3h@gmail.com>
2024-03-17 03:27:43 +0530

    Update URL

--- app/src/main/java/com/matthaigh27/chatgptwrapper/MainActivity.kt
+++ app/src/main/java/com/matthaigh27/chatgptwrapper/MainActivity.kt
@@ -21,7 +21,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 class MainActivity : Activity() {
     private val userAgent =
         "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.135 Mobile Safari/537.36"
-    private val chatUrl = "https://chat.openai.com/"
+    private val chatUrl = "https://chat.olakrutrim.com/"
     private lateinit var binding: ActivityMainBinding
     private lateinit var webView: WebView
     private lateinit var swipeLayout: SwipeRefreshLayout
